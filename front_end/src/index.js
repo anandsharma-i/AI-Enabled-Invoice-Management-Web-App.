5fe0e516977c9af402ede36a39e594125c4512bc
Anand Sharma <62071424+anandsharma-i@users.noreply.github.com>
2022-06-28 15:20:35 +0530

    Delete front_end directory

--- front_end/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-
-//NOTE: ALL UI SCREENSHOT ARE IN THE SRC/RESOURCES/UI-ScreenShots folder
-
-ReactDOM.render(<App />,document.getElementById('root'));
